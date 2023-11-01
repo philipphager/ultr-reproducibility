aa30350a664280095091079dd12304a829cd316d
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-01 13:49:45 +0100

    Reduce number of position embeddings in PBM (max positions is 22)

--- src/models/pbm.py
+++ src/models/pbm.py
@@ -17,7 +17,7 @@ class PositionBasedModel(nn.Module):
         self, batch, training: bool = False
     ) -> Union[Array, Tuple[Array, Array]]:
         relevance_model = Tower(layers=[16, 16])
-        examination_model = nn.Embed(num_embeddings=100, features=1)
+        examination_model = nn.Embed(num_embeddings=50, features=1)
         relevance = relevance_model(batch["query_document_embedding"]).squeeze()
 
         if training:
