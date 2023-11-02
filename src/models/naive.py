3ec66b09fef22824134ce0fd37859995bb2573d2
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-02 14:01:34 +0100

    Update naive.py

--- src/models/naive.py
+++ src/models/naive.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
 from flax import linen as nn
 from jax import Array
@@ -7,10 +7,16 @@ from src.models.base import Tower
 
 
 class NaiveModel(nn.Module):
+    relevance_layers: List[int]
+    relevance_dropouts: List[float]
+
     @nn.compact
     def __call__(
         self, batch, training: bool = False
     ) -> Union[Array, Tuple[Array, Array]]:
-        relevance_model = Tower(layers=[16, 16], dropouts=[0.5, 0.5])
+        relevance_model = Tower(
+            layers=self.relevance_layers,
+            dropouts=self.relevance_dropouts,
+        )
         relevance = relevance_model(batch["query_document_embedding"], training)
         return relevance.squeeze()
