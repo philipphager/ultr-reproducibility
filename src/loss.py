346cfae2814b9afde8915625cc0e0db7354ff8b9
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-05 15:03:26 +0100

    Update loss.py

--- src/loss.py
+++ src/loss.py
@@ -1,12 +1,9 @@
-from typing import Optional
-
 import jax.numpy as jnp
 import rax
 from flax import linen as nn
 from jax import Array
 from jax._src.lax.lax import stop_gradient
-from rax._src import utils
-from rax._src.types import ReduceFn, LossFn
+from rax._src.types import LossFn
 
 
 def regression_em(
@@ -14,7 +11,6 @@ def regression_em(
     labels: Array,
     where: Array,
     loss_fn: LossFn = rax.pointwise_sigmoid_loss,
-    reduce_fn: Optional[ReduceFn] = jnp.mean,
 ) -> Array:
     """
     Implementation of RegressionEM from Wang et al, 2018: https://research.google/pubs/pub46485/
@@ -36,15 +32,12 @@ def regression_em(
         exam_logits,
         exam_posterior,
         where=where,
-        reduce_fn=None,
     )
 
     rel_loss = loss_fn(
         rel_logits,
         rel_posterior,
         where=where,
-        reduce_fn=None,
     )
 
-    loss = exam_loss + rel_loss
-    return utils.safe_reduce(loss, where=where, reduce_fn=reduce_fn)
+    return exam_loss + rel_loss
