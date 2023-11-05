804c6da5e18e0f0625717ce3b2c56aacbe4be74c
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-05 16:49:27 +0100

    Add loss for the Dual Learning Algorithm

--- src/loss.py
+++ src/loss.py
@@ -41,3 +41,60 @@ def regression_em(
     )
 
     return exam_loss + rel_loss
+
+
+def dual_learning_algorithm(
+    scores: Array,
+    labels: Array,
+    where: Array,
+    loss_fn: LossFn = rax.softmax_loss,
+    max_weight: float = 10,
+) -> Array:
+    """
+    Implementation of the Dual Learning Algorithm from Ai et al, 2018: https://arxiv.org/pdf/1804.05938.pdf
+    """
+
+    examination, relevance = scores
+    examination_weights = stop_gradient(
+        get_normalized_weights(examination, where, max_weight)
+    )
+    relevance_weights = stop_gradient(
+        get_normalized_weights(relevance, where, max_weight)
+    )
+
+    examination_loss = loss_fn(
+        examination,
+        labels,
+        where=where,
+        weights=relevance_weights,
+    )
+    relevance_loss = loss_fn(
+        relevance,
+        labels,
+        where=where,
+        weights=examination_weights,
+    )
+
+    return examination_loss + relevance_loss
+
+
+def get_normalized_weights(
+    scores: Array,
+    where: Array,
+    max_weight: float,
+) -> Array:
+    """
+    Converts logits to normalized propensity weights by:
+    1. Applying a softmax to all scores in a ranking (ignoring masked values)
+    2. Normalizing the resulting probabilities by the first item in each ranking
+    3. Inverting propensities to obtain weights: e.g., propensity 0.5 -> weight 2
+    4. Clip the final weights to reduce variance
+    """
+    scores = jnp.where(where, scores, -jnp.ones_like(scores) * jnp.inf)
+    probabilities = nn.softmax(scores, axis=-1)
+    # Normalize propensities by the item in first position and convert propensities
+    # to weights by computing weights as 1 / propensities:
+    weights = probabilities[:, 0].reshape(-1, 1) / probabilities
+    weights = jnp.where(where, weights, jnp.ones_like(scores))
+
+    return weights.clip(max=max_weight)
