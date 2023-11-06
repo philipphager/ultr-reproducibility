f2e2f8c89bc1d0560f9e10b407075b6702ed9409
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-06 11:41:12 +0100

    Update baselines.sh

--- baselines.sh
+++ baselines.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+rm -rf multirun
 python main.py -m model=naive loss=pointwise,listwise
 python main.py -m model=pbm,two-towers model.tower_combination=ADDITIVE loss=pointwise,listwise
-python main.py -m model=pbm,two-towers model.tower_combination=NONE loss=pointwise-dla,listwise-dla,pointwise-em,listwise-em
+python main.py -m model=pbm,two-towers model.tower_combination=NONE loss=listwise-dla,pointwise-em,listwise-em
