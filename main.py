f70f44c66b167b299cf18f357e75756a575ab155
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-13 19:36:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def main(config: DictConfig):
             "dcg@10": partial(rax.dcg_metric, topn=10),
         },
         epochs=25,
-        early_stopping=EarlyStopping(metric="dcg@10", patience=1),
+        early_stopping=EarlyStopping(metric="dcg@10", patience=2),
     )
     best_state = trainer.train(model, trainer_loader, val_loader)
 
