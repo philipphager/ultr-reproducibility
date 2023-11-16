d4b5712f8489e33f26b00614f786cc0542572144
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-16 14:55:28 +0100

    Add SBATCH script for naive model

---
+++ scripts/tune_naive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=baidu-ultr
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task 8
+#SBATCH --mem 64GB
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=p.k.hager@uva.nl
+
+source ${HOME}/.bashrc
+conda activate baidu-reproducibility
+
+python main.py -m model=naive loss=pointwise +tune=naive
