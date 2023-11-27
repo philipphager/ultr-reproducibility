b2f89bb65e68bffb65de2cdbb855faffbf674bc1
Romain <romain.deffayet@naverlabs.com>
2023-11-27 16:11:34 +0100

    Removed BC metrics while they are not logged by the trainer.

--- app/utils/const.py
+++ app/utils/const.py
@@ -1,4 +1,4 @@
 MODELS = ["pbm", "two-towers", "naive"]
-METRICS = ["dcg@01", "dcg@03", "dcg@05", "dcg@10", "ndcg@10", "mrr@10", 
-           "click_loss", "BC_dcg@01", "BC_dcg@03", "BC_dcg@05", "BC_dcg@10", "BC_ndcg@10", "BC_mrr@10"]
+METRICS = ["dcg@01", "dcg@03", "dcg@05", "dcg@10", "ndcg@10", "mrr@10"] 
+           #"click_loss", "BC_dcg@01", "BC_dcg@03", "BC_dcg@05", "BC_dcg@10", "BC_ndcg@10", "BC_mrr@10"]
 LOSSES = ["listwise", "listwise-dla", "listwise-em", "pointwise", "pointwise-em", "pointwise-bc", "pairwise-bc"]
