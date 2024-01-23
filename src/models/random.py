093dd0f2d9f67aff9c89c66d678bd97541eb397a
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-23 15:46:09 +0100

    Update random.py

--- src/models/random.py
+++ src/models/random.py
@@ -1,16 +1,27 @@
-from typing import Tuple
-
 from flax import linen as nn
+from flax.struct import dataclass
 from jax import Array
 from jax import random
+
+
+@dataclass
+class RandomOutput:
+    loss: float
+    click: Array
+    relevance: Array
 
 
 class RandomModel(nn.Module):
     rng_collection: str = "random_model"
 
-    def __call__(self, batch, training: bool = False) -> Tuple[Array, Array, None]:
+    def __call__(self, batch, training: bool = False) -> RandomOutput:
         relevance = self.predict_relevance(batch, training)
-        return relevance, relevance, None
+
+        return RandomOutput(
+            loss=0,
+            click=relevance,
+            relevance=relevance,
+        )
 
     def predict_relevance(
         self,
