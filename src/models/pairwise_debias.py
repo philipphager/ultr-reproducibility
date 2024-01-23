f1b7b0fd0464b6dcfeea4127f604101afd5a8204
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-23 15:19:11 +0100

    Refactor pairwise debiasing

---
+++ src/models/pairwise_debias.py
@@ -0,0 +1,66 @@
+from typing import Dict
+
+from flax import linen as nn
+from flax.struct import dataclass
+from jax import Array
+
+from src.loss import pairwise_debiasing
+from src.models.base import (
+    RelevanceModel,
+    ExaminationModel,
+)
+
+
+@dataclass
+class PairwiseDebiasConfig:
+    dims: int
+    layers: int
+    dropout: float
+    positions: int
+
+
+@dataclass
+class PairwiseDebiasOutput:
+    loss: Array
+    ratio_positive: Array
+    ratio_negative: Array
+    relevance: Array
+
+
+class PairwiseDebiasModel(nn.Module):
+    """
+    With document d, query q, at position k:
+    P(C = 1 | d, q, k) = t+(k) x P(R = 1 | d, q)
+    P(C = 0 | d, q, k) = t-(k) x P(R = 0 | d, q)
+    """
+
+    config: PairwiseDebiasConfig
+
+    def setup(self):
+        config = self.config
+        self.relevance_model = RelevanceModel(config)
+        self.bias_model_positive = ExaminationModel(positions=config.positions)
+        self.bias_model_negative = ExaminationModel(positions=config.positions)
+
+    def __call__(self, batch: Dict, training: bool) -> PairwiseDebiasOutput:
+        relevance = self.predict_relevance(batch, training=training)
+        ratio_positive = self.bias_model_positive(batch, training=training)
+        ratio_negative = self.bias_model_negative(batch, training=training)
+
+        loss = pairwise_debiasing(
+            ratio_positive=ratio_positive,
+            ratio_negative=ratio_negative,
+            relevance=relevance,
+            labels=batch["click"],
+            where=batch["mask"],
+        )
+
+        return PairwiseDebiasOutput(
+            loss=loss,
+            ratio_positive=ratio_positive,
+            ratio_negative=ratio_negative,
+            relevance=relevance,
+        )
+
+    def predict_relevance(self, batch: Dict, training: bool = False) -> Array:
+        return self.relevance_model(batch, training=training)
