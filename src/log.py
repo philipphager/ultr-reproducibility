fa19ca4b884de45d5dc72ba8d28c011fd8bbf5e0
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-23 20:00:14 +0100

    Update log.py

--- src/log.py
+++ src/log.py
@@ -19,8 +19,7 @@ def print_metric_table(metrics: Dict, title=""):
 
 def get_wandb_run_name(config: DictConfig) -> str:
     model = config.model._target_.split('.')[-1]
-    loss = config.loss._target_.split('.')[-1]
     random_state = config.random_state
     timestamp = int(time.time())
 
-    return f"{model}__{loss}__{random_state}__{timestamp}"
+    return f"{model}__{random_state}__{timestamp}"
