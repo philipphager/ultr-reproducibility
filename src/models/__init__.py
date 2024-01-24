9ae5b8e2eb97f7b7fe23def2815fb97362eb8172
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-24 18:43:32 +0100

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,3 @@
-from .behavior_cloning import BehaviorCloningModel, BehaviorCloningConfig
 from .dla import DualLearningAlgorithm, DLAConfig
 from .ips import IPSModel, IPSConfig
 from .naive import NaiveModel, NaiveConfig
