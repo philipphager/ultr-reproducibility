9dc5eb1ef2dfb19503c948f76a26fb7d0d1f66df
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-24 09:10:38 +0100

    Update log.py

--- src/log.py
+++ src/log.py
@@ -1,20 +1,6 @@
 import time
-from typing import Dict
 
 from omegaconf import DictConfig
-from rich.console import Console
-from rich.table import Table
-
-
-def print_metric_table(metrics: Dict, title=""):
-    console = Console()
-    table = Table(show_header=True, header_style="bold", title=title, min_width=32)
-
-    for name in metrics.keys():
-        table.add_column(name)
-
-    table.add_row(*list(map(str, metrics.values())))
-    console.print(table)
 
 
 def get_wandb_run_name(config: DictConfig) -> str:
