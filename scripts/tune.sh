98c29d001e73f9612c25e47d71228266fae99597
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-24 09:11:21 +0100

    Drop scripts

--- scripts/tune.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ${HOME}/.bashrc
-conda activate ultr-reproducibility
-
-python main.py -m model=naive loss=pointwise +tune=naive $@
-python main.py -m model=pbm loss=pointwise +tune=naive $@
-python main.py -m model=two-towers loss=pointwise +tune=naive $@
