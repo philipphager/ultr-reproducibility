d096be4da3e228f37bf690e8cc81f67108d679f7
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-02-01 16:19:37 +0100

    Add notebooks

---
+++ notebooks/theme.py
@@ -0,0 +1,31 @@
+def theme():
+    return {
+        "config": {
+             "title": {
+                "font": "serif",
+                "fontWeight": "normal",
+                "fontSize": 16,
+                "dx": 5
+            },
+            "axis": {
+                "titleFont": "serif",
+                "titleFontWeight": "normal",
+                "titleFontSize": 16,
+                "labelFont": "serif",
+                "labelFontWeight": "normal",
+                "labelFontSize": 16
+            },
+            "headerColumn": {
+                "titleFont": "serif",
+                "titleFontWeight": "normal",
+                "titleFontSize": 16,
+                "labelFont": "serif",
+                "labelFontWeight": "normal",
+                "labelFontSize": 16
+            },
+            "text": {
+                "font": "serif",
+                "fontSize": 14,
+            }
+        },
+    }
