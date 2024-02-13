86200bb34451c4b49f482805dbcd8f671cd20ae4
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-02-13 14:17:07 +0100

    Drop streamlit app

--- app/utils/model.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from flax.training import checkpoints
-
-
-def load_state(path: Path, name: str = "best_state"):
-    return checkpoints.restore_checkpoint(path.resolve() / name, target=None)
